e738f3bcc77fdc73b2694242c3657e537d5ec29e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-16 22:08:46 +0100

    feat: add basic server code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+func handleConnection(conn *net.TCPConn) {
+	fmt.Println(conn)
+}
+
 func main() {
-	fmt.Println("Hi there!")
+	l, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:   []byte{127, 0, 0, 1},
+		Port: 3300,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := l.AcceptTCP()
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Accept new connection
+		go handleConnection(conn)
+	}
 }
