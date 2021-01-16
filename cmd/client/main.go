df04749a0f702de42716e95f60b82589a73cdd05
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-16 22:37:27 +0100

    feat(client): add basic client-side code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net"
+)
+
 func main() {
-	// TODO:
+	conn, err := net.DialTCP("tcp", nil, &net.TCPAddr{
+		IP:   []byte{127, 0, 0, 1},
+		Port: 3300,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Write([]byte("Hi there! ~Kees"))
+	conn.Close()
 }
