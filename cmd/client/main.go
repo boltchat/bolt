72e52cd5d4a6d56b051be5629d55d63c5d20c61e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-18 23:13:17 +0100

    refactor: move prompt logic to internals

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"keesvv/go-tcp-chat/internals/message"
-	"keesvv/go-tcp-chat/internals/user"
+	"keesvv/go-tcp-chat/internals/client"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -23,21 +20,6 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Print("> ")
-		content, readError := reader.ReadString('\n')
-		content = strings.TrimSpace(content)
-
-		if readError != nil {
-			panic(readError)
-		}
-
-		msg := message.Message{
-			Content: content,
-			User: &user.User{
-				Nickname: "Kees",
-			},
-		}
-
-		msg.Send(conn)
+		client.Prompt(reader, conn)
 	}
 }
---
+++ internals/client/prompt.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"bufio"
+	"fmt"
+	"keesvv/go-tcp-chat/internals/message"
+	"keesvv/go-tcp-chat/internals/user"
+	"net"
+	"strings"
+)
+
+/*
+Prompt prompts the user for sending messages.
+*/
+func Prompt(r *bufio.Reader, conn *net.TCPConn) {
+	fmt.Print("> ")
+	content, readError := r.ReadString('\n')
+	content = strings.TrimSpace(content)
+
+	if readError != nil {
+		panic(readError)
+	}
+
+	msg := message.Message{
+		Content: content,
+		User: &user.User{
+			Nickname: "Kees",
+		},
+	}
+
+	msg.Send(conn)
+}
