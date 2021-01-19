2fa755d49601291adb4e61ff154b1cb0c3dcf603
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-19 20:25:28 +0100

    feat(logger): include timestamp in logs

--- internals/logging/logger.go
+++ internals/logging/logger.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/fatih/color"
 )
@@ -11,21 +12,21 @@ func logBase(
 	level string,
 	msg string,
 ) {
-	fmt.Printf("[%s] %s\n", level, msg)
+	fmt.Printf("%s [%s] %s\n", color.HiBlackString(time.Now().Format("15:04:05")), level, msg)
 }
 
-func logInfo(msg string) {
+func LogInfo(msg string) {
 	logBase(color.CyanString("INFO"), msg)
 }
 
 func LogListener(ip string, port int) {
-	logInfo(fmt.Sprintf("Server listening on %s:%v.", ip, port))
+	LogInfo(fmt.Sprintf("Server listening on %s:%v.", ip, port))
 }
 
 func LogConnection(conn *net.TCPConn) {
-	logInfo(fmt.Sprintf("%s connected! Say hi.", conn.RemoteAddr().String()))
+	LogInfo(fmt.Sprintf("%s connected! Say hi.", conn.RemoteAddr().String()))
 }
 
 func LogDisconnect(conn *net.TCPConn) {
-	logInfo(fmt.Sprintf("%s disconnected.", conn.RemoteAddr().String()))
+	LogInfo(fmt.Sprintf("%s disconnected.", conn.RemoteAddr().String()))
 }
