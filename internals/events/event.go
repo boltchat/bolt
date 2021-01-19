05e731a96d54942299de4e6a80baefe36d7138ea
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-19 14:47:13 +0100

    feat(events): include CreatedAt

--- internals/events/event.go
+++ internals/events/event.go
@@ -1,15 +1,19 @@
 package events
 
+import "time"
+
 // Type represents an event type identifier.
 type Type int
 
 const (
+	// MessageType TODO
 	MessageType Type = iota
 )
 
 // Event represents a chat event.
 type Event struct {
-	Type Type `json:"t"`
+	Type      Type  `json:"t"`
+	CreatedAt int64 `json:"c"`
 }
 
 // BaseEvent TODO
@@ -19,6 +23,7 @@ type BaseEvent struct {
 
 func newEvent(t Type) *Event {
 	return &Event{
-		Type: t,
+		Type:      t,
+		CreatedAt: time.Now().Unix(),
 	}
 }
