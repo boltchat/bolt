6a98c695e3a15e9456a2b516fd0d049dc10a2f3a
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-19 11:45:42 +0100

    feat(logger): add LogDisconnect method

--- internals/logging/logger.go
+++ internals/logging/logger.go
@@ -8,3 +8,7 @@ import (
 func LogConnection(conn *net.TCPConn) {
 	fmt.Printf("%s connected! Say hi.\n", conn.RemoteAddr().String())
 }
+
+func LogDisconnect(conn *net.TCPConn) {
+	fmt.Printf("%s disconnected.\n", conn.RemoteAddr().String())
+}
