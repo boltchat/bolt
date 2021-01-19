6775b2631c63d1f19c401c2e88c69513f9a01155
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-19 11:54:01 +0100

    feat(events): add event.go

---
+++ internals/event/event.go
@@ -0,0 +1,14 @@
+package event
+
+// Type represents an event type identifier.
+type Type int
+
+const (
+	// MessageEvent represents a message event.
+	MessageEvent Type = iota
+)
+
+// Event represents a chat event.
+type Event struct {
+	Type Type `json:"t"`
+}
