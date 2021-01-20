2c044f2768a47d458214b09097c71b7dface7723
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-20 11:56:39 +0100

    refactor(events): use struct embedding

--- internals/events/join.go
+++ internals/events/join.go
@@ -3,13 +3,15 @@ package events
 import "keesvv/go-tcp-chat/internals/user"
 
 type JoinEvent struct {
-	Event *Event     `json:"e"`
-	User  *user.User `json:"user"`
+	BaseEvent
+	User *user.User `json:"user"`
 }
 
 func NewJoinEvent(user *user.User) *JoinEvent {
 	return &JoinEvent{
-		Event: newEvent(JoinType),
-		User:  user,
+		BaseEvent: BaseEvent{
+			Event: newEvent(JoinType),
+		},
+		User: user,
 	}
 }
--- internals/events/message.go
+++ internals/events/message.go
@@ -4,14 +4,16 @@ import "keesvv/go-tcp-chat/internals/message"
 
 // MessageEvent TODO
 type MessageEvent struct {
-	Event   *Event           `json:"e"`
+	BaseEvent
 	Message *message.Message `json:"msg"`
 }
 
 // NewMessageEvent TODO
 func NewMessageEvent(msg *message.Message) *MessageEvent {
 	return &MessageEvent{
-		Event:   newEvent(MessageType),
+		BaseEvent: BaseEvent{
+			Event: newEvent(MessageType),
+		},
 		Message: msg,
 	}
 }
