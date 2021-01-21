80cb4936d4218ad9e21a16cfe98d2147ffdbba3b
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-21 11:26:48 +0100

    feat(prompt): draw user input

--- internals/client/prompt.go
+++ internals/client/prompt.go
@@ -7,11 +7,28 @@ import (
 	"github.com/gdamore/tcell/v2/encoding"
 )
 
-func displayPrompt(s tcell.Screen) {
-	_, h := s.Size()
+func displayPrompt(s tcell.Screen, input []rune) {
+	w, h := s.Size()
 	style := tcell.StyleDefault.Foreground(tcell.ColorYellow).Bold(true)
-	s.SetContent(0, h-1, '>', nil, style)
-	s.Show()
+	y := h - 1
+
+	// Clear prompt line
+	for i := 0; i < w; i++ {
+		s.SetContent(i, y, ' ', nil, tcell.StyleDefault)
+	}
+
+	// Print prompt arrow
+	s.SetContent(0, y, '>', nil, style)
+
+	// Print user input
+	for i := 0; i < len(input); i++ {
+		s.SetContent(i+2, y, input[i], nil, tcell.StyleDefault)
+	}
+
+	// Draw a vertical line after input
+	s.SetContent(len(input)+2, y, tcell.RuneVLine, nil, tcell.StyleDefault)
+
+	s.Sync() // TODO: optimise
 }
 
 func displayChatbox(s tcell.Screen) {
@@ -29,6 +46,7 @@ Prompt prompts the user for sending messages.
 */
 func Prompt(r *bufio.Reader, conn *Connection) {
 	encoding.Register()
+	input := make([]rune, 0, 20)
 
 	// Create a screen
 	s, err := tcell.NewScreen()
@@ -43,21 +61,29 @@ func Prompt(r *bufio.Reader, conn *Connection) {
 
 	// Set style
 	s.SetStyle(tcell.StyleDefault.Foreground(tcell.ColorWhite))
-	displayPrompt(s)
+	displayPrompt(s, input)
 	displayChatbox(s)
 
 	for {
 		switch ev := s.PollEvent().(type) {
-		case *tcell.EventResize:
-			s.Sync()
-			displayPrompt(s)
-			displayChatbox(s)
+		// case *tcell.EventResize:
+		// 	s.Sync()
+		// 	displayPrompt(s)
+		// 	displayChatbox(s)
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
 				// Exit prompt
 				s.Fini()
 				return
+			} else if ev.Key() == tcell.KeyEnter {
+				input = []rune{}
+			} else if ev.Key() == tcell.KeyBackspace2 && len(input) > 0 {
+				input = input[:len(input)-1]
+			} else {
+				input = append(input, ev.Rune())
 			}
+
+			displayPrompt(s, input)
 		}
 	}
 
