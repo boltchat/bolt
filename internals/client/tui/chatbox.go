c66bacf43a5ae8944f36c9ada106f09c7027f7fc
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-21 16:45:38 +0100

    refactor(chatbox): add printEvent method

--- internals/client/tui/chatbox.go
+++ internals/client/tui/chatbox.go
@@ -6,6 +6,14 @@ import (
 
 const promptOffset int = 1
 
+func printEvent(s tcell.Screen, y int, evt string) {
+	chars := []rune(evt)
+
+	for x, ch := range chars {
+		s.SetContent(x, y, ch, nil, tcell.StyleDefault)
+	}
+}
+
 func displayChatbox(s tcell.Screen, evts chan string) {
 	w, h := s.Size()
 	events := make([]string, 0, 50)
@@ -22,11 +30,7 @@ func displayChatbox(s tcell.Screen, evts chan string) {
 
 		// Append all event to the chatbox
 		for y, line := range events {
-			chars := []rune(line)
-
-			for x, ch := range chars {
-				s.SetContent(x, y, ch, nil, tcell.StyleDefault)
-			}
+			printEvent(s, y, line)
 		}
 
 		s.Show()
