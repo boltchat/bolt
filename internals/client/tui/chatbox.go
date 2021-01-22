977c49013aaaed9709e1f26330c8a11ef9106709
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-22 11:11:29 +0100

    feat(chatbox): add buffer to prevent events visually overflowing

--- internals/client/tui/chatbox.go
+++ internals/client/tui/chatbox.go
@@ -7,41 +7,57 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
-func printLine(s tcell.Screen, y int, line string) {
+func printEvent(s tcell.Screen, y int, evt string) {
 	/*
 		I do not like this workaround at all, but at this
 		point, I've given up on trying to find a better
 		solution. Feel free to create a Pull Request if
 		you're able to improve this.
 	*/
-	chars := []rune("\b\b" + line)
+	chars := []rune("\b\b" + evt)
 
 	s.SetContent(0, y, ' ', chars[1:], tcell.StyleDefault)
 }
 
 func displayChatbox(s tcell.Screen, evtChannel chan *events.BaseEvent) {
-	w, h := s.Size()
+	/*
+		Preallocate a size of 50 for both the
+		events slice and the buffer slice.
+	*/
 	evts := make([]*events.BaseEvent, 0, 50)
+	buff := make([]*events.BaseEvent, 0, 50)
 
 	for evt := range evtChannel {
+		w, h := s.Size()
+		hBuff := h - promptOffset
+
+		// Append event to the events slice
 		evts = append(evts, evt)
 
+		if len(buff) < hBuff {
+			// Append event to buffer
+			buff = append(buff, evt)
+		} else {
+			// Remove first event from buffer and append
+			buff = append(buff[1:], evt)
+		}
+
 		// Clear the chatbox
-		for y := 0; y < (h - promptOffset); y++ { // TODO
+		for y := 0; y < hBuff; y++ {
 			for x := 0; x < w; x++ {
 				s.SetContent(x, y, ' ', nil, tcell.StyleDefault)
 			}
 		}
 
-		// Append all event to the chatbox
-		for y, event := range evts {
+		// Append all events to the chatbox buffer
+		for y, event := range buff {
 			formatMap := map[events.Type]format.FormatHandler{
 				events.MotdType:    format.FormatMotd,
 				events.MessageType: format.FormatMessage,
 				events.ErrorType:   format.FormatError,
 			}
 
-			printLine(s, y, formatMap[event.Event.Type](event))
+			printEvent(s, y, formatMap[event.Event.Type](event))
 		}
 
 		s.Show()
