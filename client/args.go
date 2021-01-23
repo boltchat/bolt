fdbfa95fb7b469c1c19904aeedcdb20d961c43f7
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-23 19:41:27 +0100

    feat(client): add command-line arguments parser

---
+++ client/args.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Args struct {
+	Hostname string
+	Port     int
+	Nickname string // TODO: this will change very soon
+}
+
+func printUsage() {
+	fmt.Println("usage: boltchat <host> <nickname>")
+}
+
+func GetArgs() *Args {
+	rawArgs := os.Args[1:]
+
+	if len(rawArgs) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	splitHost := strings.Split(rawArgs[0], ":")
+	hostname := splitHost[0]
+	port, parseErr := strconv.ParseInt(splitHost[1], 10, 32)
+
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	args := &Args{
+		Hostname: hostname,
+		Port:     int(port),
+		Nickname: rawArgs[1],
+	}
+
+	return args
+}
