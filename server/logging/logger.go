ff60c1cba9cc2b76cf6d36e70b5b059bcb95ed1c
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-24 19:19:46 +0100

    feat(logging): add LogDebug

--- server/logging/logger.go
+++ server/logging/logger.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/fatih/color"
@@ -14,11 +15,20 @@ func logBase(
 	fmt.Printf("%s [%s] %s\n", color.HiBlackString(time.Now().Format("15:04:05")), level, msg)
 }
 
-func logInfo(msg string) {
+func LogInfo(msg string) {
 	logBase(color.CyanString("INFO"), msg)
+}
+
+func LogDebug(msg string) {
+	_, isDebug := os.LookupEnv("DEBUG")
+	if !isDebug {
+		return
+	}
+
+	logBase(color.HiYellowString("DEBUG"), msg)
 }
 
 // LogListener TODO
 func LogListener(ip string, port int) {
-	logInfo(fmt.Sprintf("Server listening on %s:%v.", ip, port))
+	LogInfo(fmt.Sprintf("Server listening on %s:%v.", ip, port))
 }
