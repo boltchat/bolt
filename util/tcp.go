facc6d1f93bb5276854c6b6cf20f338c8cf06263
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-25 16:18:36 +0100

    feat(util): log pool-related events in debug mode

--- util/tcp.go
+++ util/tcp.go
@@ -3,12 +3,21 @@ package util
 import (
 	"encoding/json"
 	"net"
+
+	"github.com/bolt-chat/server/logging"
 )
 
 type ConnPool []*net.TCPConn
 
 func (c *ConnPool) AddToPool(conn *net.TCPConn) {
 	*c = append(*c, conn)
+
+	logging.LogDebug(
+		"connection added to pool:",
+		conn.RemoteAddr().String(),
+	)
+
+	logging.LogDebug("pool size:", len(*c))
 }
 
 func WriteJson(conn *net.TCPConn, data interface{}) {
