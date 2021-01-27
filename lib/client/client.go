6fa15f15a7db7c4074acfbac5ad2bd8e1bf35f21
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-27 21:11:46 +0100

    feat(client): add support for SRV records (closes #15)

--- lib/client/client.go
+++ lib/client/client.go
@@ -39,20 +39,41 @@ func NewClient(opts Options) *Client {
 	}
 }
 
+// TODO: refactor
 func (c *Client) Connect() error {
-	ips, lookupErr := net.LookupIP(c.Opts.Hostname)
-	if lookupErr != nil {
-		return lookupErr
+	var ip net.IP
+	var port int = c.Opts.Port
+
+	if parsedIP := net.ParseIP(c.Opts.Hostname); parsedIP != nil {
+		ip = parsedIP
 	}
 
-	ip := ips[0]
-	conn, err := net.DialTCP("tcp", nil, &net.TCPAddr{
+	if ip == nil {
+		_, srvAddrs, _ := net.LookupSRV("bolt", "tcp", c.Opts.Hostname)
+
+		if len(srvAddrs) > 0 {
+			targetIps, _ := net.LookupIP(srvAddrs[0].Target)
+			ip = targetIps[0]
+			port = int(srvAddrs[0].Port)
+		}
+	}
+
+	if ip == nil {
+		ips, lookupErr := net.LookupIP(c.Opts.Hostname)
+		if lookupErr != nil {
+			return lookupErr
+		}
+
+		ip = ips[0]
+	}
+
+	conn, dialErr := net.DialTCP("tcp", nil, &net.TCPAddr{
 		IP:   ip,
-		Port: c.Opts.Port,
+		Port: port,
 	})
 
-	if err != nil {
-		return err
+	if dialErr != nil {
+		return dialErr
 	}
 
 	// Set the connection
