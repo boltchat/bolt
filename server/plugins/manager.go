8788d12b188f2836dac8908e0c69ba9c098b89cc
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-28 17:43:32 +0100

    feat(plugins): add PluginManager

---
+++ server/plugins/manager.go
@@ -0,0 +1,39 @@
+// bolt.chat
+// Copyright (C) 2021  The bolt.chat Authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package plugins
+
+var manager *PluginManager
+
+type PluginManager struct {
+	installedPlugins *[]Plugin
+}
+
+func (p *PluginManager) Install(plugins ...Plugin) {
+	p.installedPlugins = &plugins
+}
+
+func (p *PluginManager) GetInstalled() *[]Plugin {
+	return p.installedPlugins
+}
+
+func SetManager(mgr *PluginManager) {
+	manager = mgr
+}
+
+func GetManager() *PluginManager {
+	return manager
+}
