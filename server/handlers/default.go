26590b4ed294c068503da70bc6a10799d02f3264
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-02 15:21:31 +0100

    refactor(server): split up connect.go into handler files

---
+++ server/handlers/default.go
@@ -0,0 +1,25 @@
+// Copyright 2021 The boltchat Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handlers
+
+import (
+	"github.com/bolt-chat/protocol/errs"
+	"github.com/bolt-chat/protocol/events"
+	"github.com/bolt-chat/server/pools"
+)
+
+func HandleDefault(p *pools.ConnPool, c *pools.Connection, e *events.BaseEvent) {
+	c.Send(*events.NewErrorEvent(errs.InvalidEvent))
+}
