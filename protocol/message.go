eb53adfb67e8a30ff9389be2644f820d4216da74
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-02 19:45:35 +0100

    feat(protocol): add Fingerprint

--- protocol/message.go
+++ protocol/message.go
@@ -19,8 +19,9 @@ Message represents a message that is
 either transmitted or stored locally.
 */
 type Message struct {
-	SentAt    int64  `json:"sent"`
-	Content   string `json:"body"`
-	Signature string `json:"sig"`
-	User      *User  `json:"user"`
+	SentAt      int64  `json:"sent"`
+	Content     string `json:"body"`
+	Signature   string `json:"sig,omitempty"`
+	Fingerprint string `json:"fprint,omitempty"`
+	User        *User  `json:"user"`
 }
