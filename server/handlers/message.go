51f7a7a0cf6fa86ce610bc0d160f6b6d7920adcc
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-02 20:16:44 +0100

    refactor(msg-handler): use new `pgp` package

--- server/handlers/message.go
+++ server/handlers/message.go
@@ -17,14 +17,12 @@ package handlers
 import (
 	"encoding/hex"
 	"encoding/json"
-	"strings"
 
 	"github.com/bolt-chat/protocol/events"
 	"github.com/bolt-chat/server/logging"
+	"github.com/bolt-chat/server/pgp"
 	"github.com/bolt-chat/server/plugins"
 	"github.com/bolt-chat/server/pools"
-	"golang.org/x/crypto/openpgp/armor"
-	"golang.org/x/crypto/openpgp/packet"
 )
 
 var publicKeyHex string = `// TODO`
@@ -39,57 +37,11 @@ func HandleMessage(p *pools.ConnPool, c *pools.Connection, e *events.BaseEvent) 
 		return
 	}
 
-	sigReader := strings.NewReader(msgEvt.Message.Signature)
-	sigDecoded, decodeErr := armor.Decode(sigReader)
-
-	if decodeErr != nil {
-		c.Send(*events.NewErrorEvent(decodeErr.Error())) // TODO
-		return
-	}
-
-	pack, packErr := packet.Read(sigDecoded.Body)
-
-	if packErr != nil {
-		c.Send(*events.NewErrorEvent(packErr.Error())) // TODO
-		return
-	}
-
-	sig, ok := pack.(*packet.Signature)
-	if !ok {
-		c.Send(*events.NewErrorEvent("invalid_signature")) // TODO
-		return
-	}
-
-	pubKeyRead := strings.NewReader(publicKeyHex)
-	pubKeyDecode, pubKeyDecodeErr := armor.Decode(pubKeyRead)
-
-	if pubKeyDecodeErr != nil {
-		c.Send(*events.NewErrorEvent(pubKeyDecodeErr.Error())) // TODO
-		return
-	}
-
-	pubKeyPack, pubKeyPackErr := packet.Read(pubKeyDecode.Body)
-
-	if pubKeyPackErr != nil {
-		c.Send(*events.NewErrorEvent(pubKeyPackErr.Error())) // TODO
-		return
-	}
-
-	pubKey, ok := pubKeyPack.(*packet.PublicKey)
-	if !ok {
-		c.Send(*events.NewErrorEvent("invalid_pubkey")) // TODO
-		return
-	}
-
-	hash := sig.Hash.New()
-	_, hashErr := hash.Write([]byte(msgEvt.Message.Content))
-
-	if hashErr != nil {
-		c.Send(*events.NewErrorEvent(hashErr.Error())) // TODO
-		return
-	}
-
-	verifyErr := pubKey.VerifySignature(hash, sig)
+	pubKey, verifyErr := pgp.VerifyMessageSignature(
+		msgEvt.Message.Signature,
+		"//PUBLIC KEY GOES HERE//",
+		msgEvt.Message.Content,
+	)
 
 	if verifyErr != nil {
 		logging.LogDebug("Signature does not match.", nil)
