87f4476fbca3d9c7f7a97a42ab86a37a59571390
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-02 16:25:44 +0100

    feat(server): validate message signatures (WIP)

--- server/handlers/message.go
+++ server/handlers/message.go
@@ -16,21 +16,67 @@ package handlers
 
 import (
 	"encoding/json"
+	"strings"
 
 	"github.com/bolt-chat/protocol/events"
 	"github.com/bolt-chat/server/plugins"
 	"github.com/bolt-chat/server/pools"
+	"golang.org/x/crypto/openpgp/armor"
+	"golang.org/x/crypto/openpgp/packet"
 )
 
 func HandleMessage(p *pools.ConnPool, c *pools.Connection, e *events.BaseEvent) {
 	msgEvt := &events.MessageEvent{}
 	json.Unmarshal(*e.Raw, msgEvt)
-	err := plugins.GetManager().HookMessage(msgEvt, c)
+	hexDecodeErr := plugins.GetManager().HookMessage(msgEvt, c)
 
-	if err != nil {
-		c.Send(*events.NewErrorEvent(err.Error()))
+	if hexDecodeErr != nil {
+		c.Send(*events.NewErrorEvent(hexDecodeErr.Error()))
 		return
 	}
+
+	sigReader := strings.NewReader(msgEvt.Message.Signature)
+	sigDecoded, decodeErr := armor.Decode(sigReader)
+
+	if decodeErr != nil {
+		c.Send(*events.NewErrorEvent(decodeErr.Error())) // TODO
+		return
+	}
+
+	pack, packErr := packet.Read(sigDecoded.Body)
+
+	if packErr != nil {
+		c.Send(*events.NewErrorEvent(packErr.Error())) // TODO
+		return
+	}
+
+	_, ok := pack.(*packet.Signature)
+	if !ok {
+		c.Send(*events.NewErrorEvent("invalid_signature")) // TODO
+		return
+	}
+
+	// pubKeyBin, hexDecodeErr := hex.DecodeString(fprint)
+	// if hexDecodeErr != nil {
+	// 	c.Send(*events.NewErrorEvent(hexDecodeErr.Error())) // TODO
+	// 	return
+	// }
+
+	// pubKeyReader := bytes.NewReader(pubKeyBin)
+
+	// pubKeyPack, pubKeyPackErr := packet.Read(pubKeyReader)
+	// if pubKeyPackErr != nil {
+	// 	c.Send(*events.NewErrorEvent(pubKeyPackErr.Error())) // TODO
+	// 	return
+	// }
+
+	// pubKey, ok := pubKeyPack.(*packet.PublicKey)
+	// if !ok {
+	// 	c.Send(*events.NewErrorEvent("invalid_pubkey")) // TODO
+	// 	return
+	// }
+
+	// pubKey.VerifySignature()
 
 	p.Broadcast(msgEvt)
 }
