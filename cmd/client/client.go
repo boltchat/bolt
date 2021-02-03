c9f144c6ef086799d95ad4405d7efe4e9797f9eb
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-03 20:26:33 +0100

    refactor(client-entry): clean up

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -15,18 +15,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/bolt-chat/client/args"
-	cliIdentity "github.com/bolt-chat/client/cli/identity"
+	"github.com/bolt-chat/client/cli"
 	"github.com/bolt-chat/client/config"
-	"github.com/bolt-chat/client/errs"
-	"github.com/bolt-chat/client/identity"
-	"github.com/bolt-chat/client/tui"
-	"github.com/bolt-chat/lib/client"
-	"github.com/bolt-chat/protocol/events"
-	"github.com/fatih/color"
 )
 
 func main() {
@@ -34,67 +26,13 @@ func main() {
 	config.LoadConfig()
 	config.LoadIdentityList()
 
-	args := args.GetArgs()
-
-	// Attempt to read the identity
-	idEntry, identityErr := config.GetIdentityEntry(args.Identity)
-	var id *identity.Identity
-
-	// TODO: refactor
-	if identityErr == config.ErrNoSuchIdentity {
-		if !cliIdentity.AskCreate(args.Identity) {
-			os.Exit(1)
-		}
-
-		var createErr error
-		id, createErr = cliIdentity.CreateIdentity(args.Identity)
-
-		if createErr != nil {
-			errs.Identity(createErr)
-		}
-	} else if identityErr != nil {
-		errs.Identity(identityErr)
-	} else {
-		var loadErr error
-		id, loadErr = identity.LoadIdentity(idEntry)
-
-		if loadErr != nil {
-			errs.Identity(loadErr)
-		}
+	cmd, cmdErr := cli.ParseCommand(os.Args[1:])
+	if cmdErr != nil {
+		cli.HandleCommandError(cmdErr)
 	}
 
-	if id.Nickname == "" {
-		errs.General(
-			fmt.Sprintf(
-				"It looks like you haven't set your nickname "+
-					"yet.\nPlease do so by editing the %s field in %s.",
-				color.HiYellowString("nickname"),
-				config.IdentityFile.GetLocation(),
-			),
-		)
+	execErr := cmd.Execute()
+	if execErr != nil {
+		cli.HandleCommandError(execErr)
 	}
-
-	c := client.NewClient(client.Options{
-		Hostname: args.Hostname,
-		Port:     args.Port,
-		Identity: id,
-	})
-
-	err := c.Connect()
-
-	if err != nil {
-		errs.Connect(err)
-	}
-
-	evts := make(chan *events.BaseEvent)
-
-	serverClosed := make(chan bool)
-	go c.ReadEvents(evts, serverClosed)
-	go tui.Display(c, evts)
-
-	// Quit when the server closes
-	<-serverClosed
-	tui.Quit()
-	fmt.Println("The server closed.")
-	os.Exit(0)
 }
