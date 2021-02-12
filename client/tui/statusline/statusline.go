967ebc3b3e07ff14811bb4df426c26e90dfc2abd
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-13 00:30:55 +0100

    feat(client): display typing indicators (WIP)

--- client/tui/statusline/statusline.go
+++ client/tui/statusline/statusline.go
@@ -17,12 +17,57 @@ package statusline
 import (
 	"github.com/boltchat/client"
 	"github.com/boltchat/client/tui/util"
+	"github.com/boltchat/protocol"
 	"github.com/boltchat/protocol/events"
 	"github.com/fatih/color"
 	"github.com/gdamore/tcell/v2"
+	"github.com/mitchellh/mapstructure"
 )
 
+var typingMap map[protocol.User]bool
+
 func DisplayStatusLine(s tcell.Screen, evtsChan chan *events.Event) {
+	typingMap = make(map[protocol.User]bool, 3)
+
 	util.PrintLine(s, 0, 0, color.CyanString("Bolt Client v%s", client.Version.VersionString))
 	s.Show()
+
+	for evt := range evtsChan {
+		if evt.Meta.Type != events.TypingType {
+			continue
+		}
+
+		typingData := events.TypingData{}
+		mapstructure.Decode(evt.Data, &typingData)
+
+		if typingData.IsTyping {
+			typingMap[*typingData.User] = true
+		} else {
+			delete(typingMap, *typingData.User)
+		}
+
+		w, _ := s.Size()
+		util.ClearLine(s, 0, w)
+
+		usernames := make([]string, 0, 3)
+		for user := range typingMap {
+			usernames = append(usernames, user.Nickname)
+		}
+
+		var typingText string
+
+		if len(usernames) == 1 {
+			typingText = color.CyanString("%s is typing...", usernames[0])
+		} else if len(usernames) == 2 {
+			typingText = color.CyanString("%s and %s are typing...", usernames[0], usernames[1])
+		} else if len(usernames) > 2 {
+			typingText = color.CyanString("Multiple people are typing...")
+		}
+
+		if typingText != "" {
+			util.PrintLine(s, 0, 0, typingText)
+		}
+
+		s.Sync()
+	}
 }
