afc04fd8453a8df58fa398d82304b4e4844e1ee6
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-03-05 23:42:27 +0100

    docs(events): add docs to new event types

--- protocol/v2/events/type.go
+++ protocol/v2/events/type.go
@@ -3,12 +3,20 @@ package events
 type EventType uint8
 
 const (
-	JoinEvent    EventType = 0x01
-	LeaveEvent   EventType = 0x02
+	// JoinEvent is the event type used for new connections.
+	JoinEvent EventType = 0x01
+	// LeaveEvent is the event type used for disconnects.
+	LeaveEvent EventType = 0x02
+	// MessageEvent is the event type used for messages.
 	MessageEvent EventType = 0x03
+	// CommandEvent is the event type used for commands.
 	CommandEvent EventType = 0x04
-	ErrorEvent   EventType = 0x05
-	MOTDEvent    EventType = 0x06
-	NoticeEvent  EventType = 0x07
-	TypingEvent  EventType = 0x08
+	// ErrorEvent is the event type used for error reporting.
+	ErrorEvent EventType = 0x05
+	// MOTDEvent is the event type used for the Message-of-the-Day (MOTD).
+	MOTDEvent EventType = 0x06
+	// NoticeEvent is the event type used for general server notices.
+	NoticeEvent EventType = 0x07
+	// TypingEvent is the event type used for typing indicators.
+	TypingEvent EventType = 0x08
 )
